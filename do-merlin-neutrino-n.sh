6c3b4d7a78f869793acdf77b54f63cb135705a7c
ZyCromerZ <neetroid97@gmail.com>
2021-02-18 20:50:16 +0700

    add merlin
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ do-merlin-neutrino-n.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+branch="20210216/neutrino-n"
+BuilderKernel="clang"
+# UseOldGcc="Y"
+. main-merlin.sh 'initial' 
+export KBUILD_BUILD_VERSION=2
+
+spectrumFile="none"
+TypeBuild="Stable"
+TypeBuildTag="NgeTeh"
+getInfo ">> Building kernel . . . . <<"
+FolderUp="keqing-drive"
+# FolderUp="letoy-stock"
+# doSFUp=$FolderUp
+# doOsdnUp=$FolderUp
+# PostLinkNow="Y"
+
+# kDLi='stock-memeui-clang'
+CompileKernel
+
+# BuilderKernel="dtc"
+# changeGcc
+# changeClang
+
+# # kDLi='stock-memeui-dtc'
+# CompileKernel
+
+# BuilderKernel="gcc"
+# changeGcc
+# changeClang
+
+# # kDLi='stock-memeui-gcc'
+# CompileKernel
