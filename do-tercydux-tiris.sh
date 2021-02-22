ed52c2aad93a8a52d289cc959b3c3ddbb78906bf
ZyCromerZ <neetroid97@gmail.com>
2021-02-22 23:59:56 +0700

    test test
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- do-tercydux-tiris.sh
+++ do-tercydux-tiris.sh
@@ -1,20 +1,49 @@
 #! /bin/bash
-branch="20210205/neutrino-a"
+branch="20210205/neutrino-1"
 BuilderKernel="clang"
 
 . main.sh 'initial' 'full'
 export KBUILD_BUILD_VERSION=4
 
 spectrumFile="bego-on-p.rc"
-TypeBuild="Stable"
-TypeBuildTag="NON-CFW"
+TypeBuild="Stock-LMK"
+TypeBuildTag="NON-CFW-TEST"
 getInfo ">> Building kernel . . . . <<"
-FolderUp="keqing-drive"
-# PostLinkNow="Y"
+FolderUp="begonia-test"
+PostLinkNow="Y"
 # ExFolder="Hu"
 
 CompileKernel
-pullLmkB
+# pullLmkB
+# CompileKernel
+# pullSlmkB
+# CompileKernel
+
+branch="20210205/neutrino-2"
+ChangeBranch
+TypeBuild="Stock-LMK"
 CompileKernel
-pullSlmkB
+
+
+branch="20210205/neutrino-3"
+ChangeBranch
+TypeBuild="Stock-LMK"
+CompileKernel
+
+
+branch="20210205/neutrino-4"
+ChangeBranch
+TypeBuild="Stock-LMK"
+CompileKernel
+
+
+branch="20210205/neutrino-5"
+ChangeBranch
+TypeBuild="Stock-LMK"
+CompileKernel
+
+
+branch="20210205/neutrino-6"
+ChangeBranch
+TypeBuild="Stock-LMK"
 CompileKernel
