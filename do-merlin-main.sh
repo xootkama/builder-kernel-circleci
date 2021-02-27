0af5e27797fd2664f467a06cb827fd64cbb69b81
ZyCromerZ <neetroid97@gmail.com>
2021-02-27 19:55:31 +0700

    ok
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ do-merlin-main.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+branch="20210216/main"
+BuilderKernel="clang"
+# UseOldGcc="Y"
+. main-merlin.sh 'initial' 
+export KBUILD_BUILD_VERSION=99
+
+spectrumFile="none"
+TypeBuild="MAIN"
+TypeBuildTag="NgeTeh"
+getInfo ">> Building kernel . . . . <<"
+FolderUp="keqing-drive"
+# FolderUp="letoy-stock"
+# doSFUp=$FolderUp
+# doOsdnUp=$FolderUp
+# PostLinkNow="Y"
+
+# kDLi='stock-memeui-clang'
+CompileKernel
+
+# BuilderKernel="dtc"
+# changeGcc
+# changeClang
+
+# # kDLi='stock-memeui-dtc'
+# CompileKernel
+
+# BuilderKernel="gcc"
+# changeGcc
+# changeClang
+
+# # kDLi='stock-memeui-gcc'
+# CompileKernel
