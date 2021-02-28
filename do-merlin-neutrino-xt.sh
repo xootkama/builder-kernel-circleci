f851019ba27e92b9a1dddd193d4eaed34258615b
ZyCromerZ <neetroid97@gmail.com>
2021-02-28 17:49:54 +0700

    kk
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- do-merlin-neutrino-xt.sh
+++ do-merlin-neutrino-xt.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-branch="20210228/qk-n"
+branch="20210228/qk-l"
 BuilderKernel="clang"
 # UseOldGcc="Y"
 . main-merlin.sh 'initial' 'full'
@@ -16,9 +16,9 @@ FolderUp="keqing-drive"
 # PostLinkNow="Y"
 
 # kDLi='stock-memeui-clang'
-CompileKernel
-pullLmk
-CompileKernel
+# CompileKernel
+# pullLmk
+# CompileKernel
 pullSlmk
 CompileKernel
 
