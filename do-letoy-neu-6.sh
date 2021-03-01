11a4b04a050b3a500c7d6e0767259ec72e1af21e
ZyCromerZ <neetroid97@gmail.com>
2021-03-02 00:29:15 +0700

    ok
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ do-letoy-neu-6.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+branch="20210228/neutrino-6"
+BuilderKernel="dtc"
+# UseOldGcc="Y"
+. main-letoy.sh 'initial' 'full'
+# export KBUILD_BUILD_VERSION=1
+
+spectrumFile="none"
+TypeBuild="Stock-LMK"
+TypeBuildTag="Stable"
+getInfo ">> Building kernel . . . . <<"
+FolderUp="letoy-neutrino"
+doSFUp=$FolderUp
+PostLinkNow="Y"
+
+# kDLi='stock-memeui-clang'
+CompileKernel
+pullLmk
+CompileKernel
+pullSlmk
+CompileKernel
---
+++ do-letoy-qk-l.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+branch="20210228/qk-l"
+BuilderKernel="gcc"
+# UseOldGcc="Y"
+. main-letoy.sh 'initial' 'full'
+# export KBUILD_BUILD_VERSION=1
+
+spectrumFile="none"
+TypeBuild="Stock-LMK"
+TypeBuildTag="Stable"
+getInfo ">> Building kernel . . . . <<"
+FolderUp="letoy-qk"
+doSFUp=$FolderUp
+PostLinkNow="Y"
+
+# kDLi='stock-memeui-clang'
+CompileKernel
+pullLmk
+CompileKernel
+pullSlmk
+CompileKernel
