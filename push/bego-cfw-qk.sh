9b355994cf434e43168c24974ce353d18265b114
ZyCromerZ <neetroid97@gmail.com>
2021-09-19 02:04:04 +0700

    up for 'begonia-q-oss-neutrino-1'
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- push/bego-cfw-qk.sh
+++ push/bego-cfw-qk.sh
@@ -4,7 +4,7 @@ git checkout master
 if [ ! -z "$2" ];then
     ListBranch="$2"
 else
-    ListBranch="begonia-q-oss-myboy begonia-cfw-myboy"
+    ListBranch="begonia-cfw-qk-l begonia-cfw-qk-n begonia-cfw-qk-l-gcc begonia-cfw-qk-n-gcc begonia-cfw-qk-l-dtc begonia-cfw-qk-n-dtc"
 fi
 
 for Branch in $ListBranch
@@ -18,7 +18,7 @@ done
 if [ ! -z "$1" ];then
     repo="$1"
 else
-    repo="zero"
+    repo="neet"
 fi
 git push -f "$repo" $ListBranch
 
